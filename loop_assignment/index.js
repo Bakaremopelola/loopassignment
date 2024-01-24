bd3f29df321aef975759b837c961c4c79f408bd0
Bakaremopelola <152390768+Bakaremopelola@users.noreply.github.com>
2024-01-24 16:45:49 +0100

    Add files via upload

---
+++ loop_assignment/index.js
@@ -0,0 +1,19 @@
+//let i=1;
+
+//while (i <= 100) {
+    //if (i%2 == 0){
+    //    console.log(i)
+    //}
+    //i++
+//}
+
+function printevennums(params) {
+    for(let i =0; i<= 100; i+=2){
+        console.log(i);
+    }
+}
+printevennums()
+
+let fntn = document.getElementById('btn1');
+let result =document.getElementById('btn2');
+let response =['LOVE', 'LOYALTY', 'RESPECT']
